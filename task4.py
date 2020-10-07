464bc6c1128f3cea8e460d0aa71803a0056af9ab
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-07 12:59:19 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,4 +6,9 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
-
+def isInterger(a):
+    if a == int(a):
+        b = True
+    else:
+        b = False
+    return b

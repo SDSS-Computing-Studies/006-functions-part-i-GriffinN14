756f5b311378bab24385bafbd272cde3ec8cd130
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-07 12:44:06 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,8 +6,8 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
-def largest(a,b,c,d,e,f):
-    list = [a,b,c,d,e,f]
-    list.sort()
-    g = len(list) - 1
-    return list[g]
+def largest(a):
+    a.sort()
+    g = len(a) - 1
+    return a[g]
+print (largest([5,1,12.3]))

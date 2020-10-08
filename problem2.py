7beee30e0b85edfa03c38efa9fff61c46fd286e4
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-08 11:58:11 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -8,4 +8,14 @@ Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
 import math
-
+def distance(a,b):
+    if a[0] > b [0]:
+        c = a[0] - b [0]
+    else:
+        c = b[0] - a[0]
+    if a[1] > b[1]:
+        d = a[1] - b[1]
+    else:
+        d = b[1] - a[1]
+    e = (c,d)
+    return (e)
